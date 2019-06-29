ddb7fe8f3c2d659e53a1ced50fc9866ec0a3ecf8
Gaurav Gupta <gouravgupta840@gmail.com>
2019-06-30 00:28:02 +0530

    Builder Navigation Header Component

--- app/components/Navigation/index.js
+++ app/components/Navigation/index.js
@@ -6,13 +6,30 @@
 
 import React from 'react';
 
-
 import styles from './styles.css';
 
-function Navigation() {
+function Navigation () {
   return (
+    <div className={`${styles.navigation} ${styles.shadow}`}>
     <div className={styles.navigation}>
-      This is the navigation component
+      <img className={`${styles.image} ${styles.shakehand}`} src="https://res.cloudinary.com/dcalvdelc/image/upload/v1561832703/hand2.png"/>
+      <h3 className={styles.brand}>
+        {/* Brand Name Falls Here */}
+        Raisehand
+        <small className={styles.punch}>
+          &nbsp; &quot;meant for humans&quot;
+        </small>
+      </h3>
+    </div>
+      <div className={styles.navigation}>
+        <ul>
+          <li><a href="#findProduct">Find Products</a></li>
+          <li><a href="#findJobs">Find Jobs</a></li>
+          <li><a href="#findContacts">Find Peoples</a></li>
+          <li><a href="#myContacts">My Contacts</a></li>      
+        </ul>
+        <img className={styles.image} src="https://avatars2.githubusercontent.com/u/25275856?s=460&v=4" />
+      </div>
     </div>
   );
 }
